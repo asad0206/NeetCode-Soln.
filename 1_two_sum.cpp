e4d76eaa8a8e9ca66f5c8b88bde2846ac8d0de1b
Asad <asad200ana@gmail.com>
2023-08-06 17:46:15 +0530

    01.Two Sum better solution

--- 1_two_sum.cpp
+++ 1_two_sum.cpp
@@ -1,21 +1,50 @@
-//https://leetcode.com/problems/two-sum/
+// https://leetcode.com/problems/two-sum/
 
-#include<bits/stdc++.h>
-using namespace std;
+// 06-08-2023 better solution
 
-class Solution {
+class Solution
+{
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        unordered_map<int, int> m;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int dif = target - nums[i];
+            if (m.find(dif) == m.end())
+            {
+                m[nums[i]] = i;
+            }
+            else
+            {
+                return {m[dif], i};
+            }
+        }
+
+        return {};
+    }
+};
+
+//
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
         vector<int> result;
         unordered_map<int, int> m;
-        for(int i=0; i<nums.size(); i++){
+        for (int i = 0; i < nums.size(); i++)
+        {
             int tar = target - nums[i];
-            if(m.find(tar)!=m.end()){
+            if (m.find(tar) != m.end())
+            {
                 result.push_back(m[tar]);
                 result.push_back(i);
                 break;
             }
-            else{
+            else
+            {
                 m.insert({nums[i], i});
             }
         }
