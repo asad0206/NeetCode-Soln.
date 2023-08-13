ebc52b1ad4c2a3241fabcff0eb56bc4df53df8fb
Asad <asad200ana@gmail.com>
2023-08-13 15:09:36 +0530

    217.Contains duplicate | sets

--- 217_contain_duplicate.cpp
+++ 217_contain_duplicate.cpp
@@ -1,15 +1,45 @@
-//https://leetcode.com/problems/contains-duplicate/
-#include<bits/stdc++.h>
+// https://leetcode.com/problems/contains-duplicate/
+#include <bits/stdc++.h>
+
+// unordered set uses hash map
+
+class Solution
+{
+public:
+    bool containsDuplicate(vector<int> &nums)
+    {
+        unordered_set<int> s(nums.begin(), nums.end());
+        return nums.size() > s.size();
+    }
+};
+
+// set useage uses tree
+
+class Solution
+{
+public:
+    bool containsDuplicate(vector<int> &nums)
+    {
+        set<int> s(nums.begin(), nums.end());
+        return nums.size() > s.size();
+    }
+};
+
+//
 
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    bool containsDuplicate(vector<int>& nums) {
+    bool containsDuplicate(vector<int> &nums)
+    {
         sort(nums.begin(), nums.end());
-        for (auto x: nums){
+        for (auto x : nums)
+        {
             auto r = equal_range(nums.begin(), nums.end(), x);
-            if((r.second - r.first) > 1){
+            if ((r.second - r.first) > 1)
+            {
                 return true;
             }
         }
