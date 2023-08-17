36f51e857795eeaa777bb0d912a341c2abb9a1b7
Asad <asad200ana@gmail.com>
2023-08-17 20:29:50 +0530

    300.Longest increasing subsequence

---
+++ 300_longest_increasing_subsequence.cpp
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/longest-increasing-subsequence/description/
+
+class Solution
+{
+public:
+    int lengthOfLIS(vector<int> &nums)
+    {
+        vector<int> result;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            auto it = lower_bound(result.begin(), result.end(), nums[i]);
+            if (it == result.end())
+            {
+                result.push_back(nums[i]);
+            }
+            else
+            {
+                *it = nums[i];
+            }
+        }
+        return result.size();
+    }
+};
