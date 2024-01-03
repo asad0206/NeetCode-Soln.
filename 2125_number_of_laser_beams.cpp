7668e8a6cfae5848e1d184de2e0dff22fd79de88
Asad <asad200ana@gmail.com>
2024-01-03 17:00:10 +0530

    2125. Number of Laser Beams

---
+++ 2125_number_of_laser_beams.cpp
@@ -0,0 +1,68 @@
+// https://leetcode.com/problems/number-of-laser-beams-in-a-bank/description/
+
+// Difficulty: Medium
+
+// Space complexity: O(Laser beams)
+class Solution
+{
+public:
+    int numberOfBeams(vector<string> &bank)
+    {
+        if (bank.size() < 2)
+            return 0;
+
+        int lasers = 0;
+        vector<int> vec;
+
+        for (auto x : bank)
+        {
+            int count = 0;
+
+            for (auto s : x)
+            {
+                if (s == '1')
+                    count++;
+            }
+
+            if (count != 0)
+                vec.push_back(count);
+        }
+
+        for (int i = 1; i < vec.size(); i++)
+        {
+            lasers += vec[i] * vec[i - 1];
+        }
+
+        return lasers;
+    }
+};
+
+// Space complexity: O(1)
+class Solution
+{
+public:
+    int numberOfBeams(vector<string> &bank)
+    {
+        if (bank.size() < 2)
+            return 0;
+
+        int lasers = 0;
+        int lasersPrevRow = 0;
+
+        for (auto x : bank)
+        {
+            int lasersCurRow = 0;
+
+            for (auto s : x)
+            {
+                if (s == '1')
+                    lasersCurRow++;
+            }
+
+            lasers += lasersCurRow * lasersPrevRow;
+            lasersPrevRow = lasersCurRow == 0 ? lasersPrevRow : lasersCurRow;
+        }
+
+        return lasers;
+    }
+};
